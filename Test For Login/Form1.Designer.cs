af0a4f0b61ca9ade0350601af0f8f7a52245f646
John Wee <johnw@alphaconnect.com>
2021-02-04 23:35:56 -0700

    added firebase authentication

--- Test For Login/Form1.Designer.cs
+++ Test For Login/Form1.Designer.cs
@@ -28,19 +28,20 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.userNameBox = new System.Windows.Forms.TextBox();
+			this.emailBox = new System.Windows.Forms.TextBox();
 			this.passwordBox = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
 			this.PasswordLabel = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// userNameBox
+			// emailBox
 			// 
-			this.userNameBox.Location = new System.Drawing.Point(286, 67);
-			this.userNameBox.Name = "userNameBox";
-			this.userNameBox.Size = new System.Drawing.Size(171, 22);
-			this.userNameBox.TabIndex = 0;
+			this.emailBox.Location = new System.Drawing.Point(286, 67);
+			this.emailBox.Name = "emailBox";
+			this.emailBox.Size = new System.Drawing.Size(171, 22);
+			this.emailBox.TabIndex = 0;
 			// 
 			// passwordBox
 			// 
@@ -54,13 +55,13 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(100, 68);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(75, 17);
+			this.label1.Size = new System.Drawing.Size(53, 21);
 			this.label1.TabIndex = 2;
-			this.label1.Text = "UserName";
+			this.label1.Text = "Email";
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(382, 139);
+			this.button1.Location = new System.Drawing.Point(382, 136);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 3;
@@ -77,16 +78,27 @@
 			this.PasswordLabel.TabIndex = 4;
 			this.PasswordLabel.Text = "Password";
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(286, 136);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Sign Up";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.PasswordLabel);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.passwordBox);
-			this.Controls.Add(this.userNameBox);
+			this.Controls.Add(this.emailBox);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -96,11 +108,12 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox userNameBox;
+		private System.Windows.Forms.TextBox emailBox;
 		private System.Windows.Forms.TextBox passwordBox;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label PasswordLabel;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
--- Test For Login/Form1.cs
+++ Test For Login/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace Test_For_Login
 {
@@ -17,11 +20,37 @@ namespace Test_For_Login
 			InitializeComponent();
 		}
 
+		IFirebaseConfig config = new FirebaseConfig
+		{
+			AuthSecret = "82cddLuPtNIXu26qHCfjY59oGg73C6PSQjGRw3Vb",
+			BasePath = "https://cis-attempt-1-default-rtdb.firebaseio.com/"
+		};
+
 		private void button1_Click(object sender, EventArgs e)
 		{
-			string userName = userNameBox.Text;
+			string email = emailBox.Text;
 			string password = passwordBox.Text;
-			MessageBox.Show($"{userName} {password}");
+			MessageBox.Show($"{email} {password}");
+		}
+		IFirebaseClient client;
+		private void button2_Click(object sender, EventArgs e)
+		{
+			client = new FireSharp.FirebaseClient(config);
+			if (client != null)
+			{
+				MessageBox.Show("Connection Established");
+				string email = emailBox.Text;
+				string password = passwordBox.Text;
+				try
+				{
+					client.CreateUser(email, password);
+				}catch(Exception ex)
+				{
+					MessageBox.Show(ex.ToString());
+				}
+				
+				MessageBox.Show($"A new user has been successfully created with the email: {email}");
+			}
 		}
 	}
 }
