704774a51beb214d18c208e2dbe0049f86999289
adamraberschool <Setup2getup!hub>
2021-02-05 16:00:07 -0700

    Fixed login and added error handling.

--- Test For Login/Form1.cs
+++ Test For Login/Form1.cs
@@ -29,13 +29,35 @@ namespace Test_For_Login
 			var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyAsFiSNedHZ6LohezUzZ-Y7FoflxRZmwWA"));
             try
 			{
-				var login = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
-				MessageBox.Show($"{login}");
+				var userCredential = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
+				var user = userCredential.User;
+				var userEmail = user.Email;
+				MessageBox.Show($"signed in as {userEmail}");
+
 			}
             catch (Exception error)
             {
-				Console.WriteLine(error.Message);
-				MessageBox.Show(error.Message);
+				if (error.Message.Contains("INVALID_EMAIL"))
+				{
+					MessageBox.Show("email must be in user@domain.com form");
+				}
+				else if (error.Message.Contains("MISSING_PASSWORD"))
+				{
+					MessageBox.Show("Must enter a password.");
+				}
+				else if (error.Message.Contains("WEAK_PASSWORD"))
+				{
+					MessageBox.Show("Password must contain at least 6 characters.");
+				}else if (error.Message.Contains("WrongPassword"))
+                {
+					MessageBox.Show("Incorrect password. Please try again.");
+                }else
+                {
+					MessageBox.Show(error.Message);
+				}
+				
+				
+
 			}
 			
 		}
@@ -48,11 +70,31 @@ namespace Test_For_Login
 			try
 			{
 				var login = await authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
-				MessageBox.Show($"{login}");
-			}catch(Exception excp)
+				MessageBox.Show($"Account created successfully");
+			}catch(Exception error)
             {
-				MessageBox.Show(excp.Message);
-            }
+				if (error.Message.Contains("INVALID_EMAIL"))
+				{
+					MessageBox.Show("email must be in user@domain.com form");
+				}
+				else if (error.Message.Contains("MISSING_PASSWORD"))
+				{
+					MessageBox.Show("Must enter a password.");
+				}
+				else if (error.Message.Contains("EMAIL_EXISTS"))
+				{
+					MessageBox.Show("That email already belongs to an account.");
+				}
+				else if (error.Message.Contains("WEAK_PASSWORD"))
+				{
+					MessageBox.Show("Password must contain at least 6 characters.");
+                }
+                else
+                {
+					MessageBox.Show(error.Message);
+				}
+				
+			}
 
 		}
 	}
