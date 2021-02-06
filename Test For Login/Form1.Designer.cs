6207a38b05ac4e65f8bc570ba827e10ffbb1c7c4
acrab <acrab@DESKTOP-RUTS0JC>
2021-02-05 17:07:14 -0700

    Cleaned up the look

--- Test For Login/Form1.Designer.cs
+++ Test For Login/Form1.Designer.cs
@@ -28,81 +28,121 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.emailBox = new System.Windows.Forms.TextBox();
-			this.passwordBox = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.PasswordLabel = new System.Windows.Forms.Label();
-			this.button2 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// emailBox
-			// 
-			this.emailBox.Location = new System.Drawing.Point(286, 67);
-			this.emailBox.Name = "emailBox";
-			this.emailBox.Size = new System.Drawing.Size(171, 22);
-			this.emailBox.TabIndex = 0;
-			// 
-			// passwordBox
-			// 
-			this.passwordBox.Location = new System.Drawing.Point(286, 95);
-			this.passwordBox.Name = "passwordBox";
-			this.passwordBox.Size = new System.Drawing.Size(171, 22);
-			this.passwordBox.TabIndex = 1;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(100, 68);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(53, 21);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "Email";
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(382, 136);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 3;
-			this.button1.Text = "Login";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// PasswordLabel
-			// 
-			this.PasswordLabel.AutoSize = true;
-			this.PasswordLabel.Location = new System.Drawing.Point(100, 100);
-			this.PasswordLabel.Name = "PasswordLabel";
-			this.PasswordLabel.Size = new System.Drawing.Size(69, 17);
-			this.PasswordLabel.TabIndex = 4;
-			this.PasswordLabel.Text = "Password";
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(286, 136);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 5;
-			this.button2.Text = "Sign Up";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.PasswordLabel);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.passwordBox);
-			this.Controls.Add(this.emailBox);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(572, 130);
+            this.emailBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(338, 38);
+            this.emailBox.TabIndex = 0;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(572, 184);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(338, 38);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(572, 329);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(338, 65);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(200, 194);
+            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(139, 32);
+            this.PasswordLabel.TabIndex = 4;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(738, 658);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(346, 54);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Create Account";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(572, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 38);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Verify Password";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 670);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(623, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Don\'t have an account? Click here to create one ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 810);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.emailBox);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -113,7 +153,10 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label PasswordLabel;
-		private System.Windows.Forms.Button button2;
-	}
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
 }
 
--- Test For Login/Form1.cs
+++ Test For Login/Form1.cs
@@ -16,7 +16,8 @@ namespace Test_For_Login
 	{
 		private FirebaseAuthProvider authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyAsFiSNedHZ6LohezUzZ-Y7FoflxRZmwWA"));
 		private Firebase.Auth.User firebaseUser;
-		public Firebase.Auth.User getFirebaseUser()
+		private string pageState = "signIn";
+		public Firebase.Auth.User GetFirebaseUser()
 		{
 			return firebaseUser;
 		}
@@ -30,73 +31,115 @@ namespace Test_For_Login
 		{
 			string email = emailBox.Text;
 			string password = passwordBox.Text;
-            try
-			{
-				var userCredential = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
-				firebaseUser = userCredential.User;
-				var userEmail = firebaseUser.Email;
-				MessageBox.Show($"signed in as {userEmail}");
-
-			}
-            catch (Exception error)
+			if (pageState == "signIn")
             {
-				if (error.Message.Contains("INVALID_EMAIL"))
+				try
 				{
-					MessageBox.Show("email must be in user@domain.com form");
-				}
-				else if (error.Message.Contains("MISSING_PASSWORD"))
-				{
-					MessageBox.Show("Must enter a password.");
-				}
-				else if (error.Message.Contains("WEAK_PASSWORD"))
-				{
-					MessageBox.Show("Password must contain at least 6 characters.");
-				}else if (error.Message.Contains("WrongPassword"))
-                {
-					MessageBox.Show("Incorrect password. Please try again.");
-                }else
-                {
-					MessageBox.Show(error.Message);
-				}
-			}
-			
-		}
+					var userCredential = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
+					firebaseUser = userCredential.User;
+					var userEmail = firebaseUser.Email;
+					MessageBox.Show($"signed in as {userEmail}");
 
-		private async void button2_Click(object sender, EventArgs e)
-		{
-			string email = emailBox.Text;
-			string password = passwordBox.Text;
-			try
-			{
-				var userCredential = await authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
-				firebaseUser = userCredential.User;
-				MessageBox.Show($"Account created for {firebaseUser.Email}");
-				
-			}catch(Exception error)
+				}
+				catch (Exception error)
+				{
+					if (error.Message.Contains("INVALID_EMAIL"))
+					{
+						MessageBox.Show("email must be in user@domain.com form");
+					}
+					else if (error.Message.Contains("MISSING_PASSWORD"))
+					{
+						MessageBox.Show("Must enter a password.");
+					}
+					else if (error.Message.Contains("WEAK_PASSWORD"))
+					{
+						MessageBox.Show("Password must contain at least 6 characters.");
+					}
+					else if (error.Message.Contains("WrongPassword"))
+					{
+						MessageBox.Show("Incorrect password. Please try again.");
+					}
+					else
+					{
+						MessageBox.Show(error.Message);
+					}
+				}
+            }
+            else
             {
-				if (error.Message.Contains("INVALID_EMAIL"))
-				{
-					MessageBox.Show("email must be in user@domain.com form");
-				}
-				else if (error.Message.Contains("MISSING_PASSWORD"))
-				{
-					MessageBox.Show("Must enter a password.");
-				}
-				else if (error.Message.Contains("EMAIL_EXISTS"))
-				{
-					MessageBox.Show("That email already belongs to an account.");
-				}
-				else if (error.Message.Contains("WEAK_PASSWORD"))
-				{
-					MessageBox.Show("Password must contain at least 6 characters.");
+				if(textBox1.Text == passwordBox.Text) {
+					try
+					{
+						var userCredential = await authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
+						firebaseUser = userCredential.User;
+						MessageBox.Show($"Account created for {firebaseUser.Email}");
+
+					}
+					catch (Exception error)
+					{
+						if (error.Message.Contains("INVALID_EMAIL"))
+						{
+							MessageBox.Show("email must be in user@domain.com form");
+						}
+						else if (error.Message.Contains("MISSING_PASSWORD"))
+						{
+							MessageBox.Show("Must enter a password.");
+						}
+						else if (error.Message.Contains("EMAIL_EXISTS"))
+						{
+							MessageBox.Show("That email already belongs to an account.");
+						}
+						else if (error.Message.Contains("WEAK_PASSWORD"))
+						{
+							MessageBox.Show("Password must contain at least 6 characters.");
+						}
+						else
+						{
+							MessageBox.Show(error.Message);
+						}
+
+					}
                 }
                 else
                 {
-					MessageBox.Show(error.Message);
-				}
-				
+					MessageBox.Show("Passwords must match.");
+                }
+
+			}
+			
+           
+			
+		}
+
+		
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+			if (pageState == "signIn")
+			{
+				pageState = "createAccount";
+				label2.Visible = true;
+				textBox1.Visible = true;
+				button3.Text = "Sign in";
+				label3.Text = "Already have an account? Click here to sign in";
+				button1.Text = "Create Account";
+			}
+            else
+            {
+				pageState = "signIn";
+				textBox1.Text = ""; 
+				label2.Visible = false;
+				textBox1.Visible = false;
+				button3.Text = "Create Account";
+				button1.Text = "Login";
+				label3.Text = "Don't have an account? Click here to create one";
 			}
 
-		}
-	}
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
